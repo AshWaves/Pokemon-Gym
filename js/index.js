ec745c937f5a65cd13d16b88c6106c018cb378a6
AshWavesCode <ashwavescode@gmail.com>
2022-11-21 13:23:07 -0500

    fix error in fetch js

--- js/index.js
+++ js/index.js
@@ -1,23 +1,31 @@
-function getCharacters(done) {
-  const results = fetch("https://pokeapi.co/api/v2/pokemon/");
-  results
-    .then((response) => response.json())
+const pokemonContainer = document.querySelector(".pokemon-container");
+
+function fetchPokemon(id) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then((res) => res.json())
     .then((data) => {
-      done(data);
+      generatepokemons(data);
     });
 }
-getCharacters(data => {
-  data.results.forEach(pokemones => {
-    const article = document.createRange().createContextualFragment(/*html*/`
-      <article>
-            <div clas="image-container">
-                <img src="${pokemones.image}" />
-            </div>
-            <h2>${pokemones.name}</h2>
-            <span>${pokemones.status}</span>
-      </article>
-      `);
-    const main = document.querySelector("main");
-    main.append(article);
-  });
-});
+
+function numbersPositionPokemons(numbers) {
+  
+  for (let i = 0; i <=numbers; i++) {
+    fetchPokemon(i);
+  }
+}
+
+function generatepokemons(pokemon) {
+  
+  const card = document.createElement("div");
+  card.classList.add("pokemon-block");
+  const name = document.createElement("p");
+  name.classList.add("name");
+  name.textContent = pokemon.name;
+  card.appendChild(name);
+  pokemonContainer.appendChild(card);
+}
+
+
+// for load first html
+window.addEventListener("load",numbersPositionPokemons(18));
