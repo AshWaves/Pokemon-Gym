a6a6a23825c8dbf5b8689762602e98f5c0f5d2d8
AshWavesCode <ashwavescode@gmail.com>
2022-11-23 04:10:25 -0500

    finish the css and start js login

---
+++ assets/js/login.js
@@ -0,0 +1,7 @@
+//global declaration of variables
+let loginForm=document.getElementById("form-login");
+let loginRegister=document.getElementById("form-register");
+//to call my form register
+function register(){
+
+}
