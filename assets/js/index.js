940a89274db79139aa3d709bcaafb89cb304deec
AshWavesCode <ashwavescode@gmail.com>
2022-11-22 22:26:01 -0500

    build login and register

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+const pokemonContainer = document.querySelector(".pokemon-container");
+
+function fetchPokemon(id) {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then((res) => res.json())
+    .then((data) => {
+      generatepokemons(data);
+    });
+}
+
+function numbersPositionPokemons(numbers) {
+  
+  for (let i = 0; i <=numbers; i++) {
+    fetchPokemon(i);
+  }
+}
+
+function generatepokemons(pokemon) {
+  
+  const card = document.createElement("div");
+  card.classList.add("pokemon-block");
+  const name = document.createElement("p");
+  name.classList.add("name");
+  name.textContent = pokemon.name;
+  card.appendChild(name);
+  pokemonContainer.appendChild(card);
+}
+
+
+// for load first html
+window.addEventListener("load",numbersPositionPokemons(20));
