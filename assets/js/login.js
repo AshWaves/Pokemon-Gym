2682271737266f228714f02a97853f214dd0d9f8
AshWavesCode <ashwavescode@gmail.com>
2022-11-23 06:42:44 -0500

    connection

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,7 +1,38 @@
+document.getElementById("btn-register").addEventListener("click",register);
+document.getElementById("btn-start-sesion").addEventListener("click",login);
+
 //global declaration of variables
 let loginForm=document.getElementById("form-login");
 let loginRegister=document.getElementById("form-register");
+let loginContainer=document.getElementById("login-container");
+let backBoxLoign=document.getElementById("back-box-login");
+let backBoxRegister=document.getElementById("back-box-register");
 //to call my form register
 function register(){
+    
+    loginRegister.style.display="block";
+    loginContainer.style.left="320px";
+    loginContainer.style.top="-420px";
+    loginForm.style.display="none";
+    backBoxRegister.style.opacity=0;
+    backBoxLoign.style.opacity="1";
+}
 
+function login(){
+    if(window.innerWidth>800){
+        loginRegister.style.display="none";
+        loginContainer.style.left="10px";
+        loginContainer.style.top="-420px";
+        loginForm.style.display="block";
+        backBoxRegister.style.opacity=1;
+        backBoxLoign.style.opacity="0";
+    }else{
+        loginRegister.style.display="none";
+    loginContainer.style.left="0";
+    loginContainer.style.top="-420px";
+    loginForm.style.display="block";
+    backBoxRegister.style.opacity=1;
+    backBoxLoign.style.opacity="0";
+    }
+    
 }
---
+++ php/contoller.php
@@ -0,0 +1,9 @@
+<?php
+    $connection=mysqli_connect("localhost","root","","Login_accedo");
+    if($connection){
+        echo "conectado exitosamente ala bs";
+    }else{
+        echo "no se ha conectado ala base de datos";
+    }
+    
+?>
